6c89f2c61d0347759caf6fafedc28b6acb243f9a
wanghaoming <whimmy@126.com>
2018-08-15 16:33:43 +0800

    use PasswordEncoder

--- spring-cloud-security-introduction/auth-server/src/main/java/com/ymmihw/spring/cloud/security/introduction/auth/server/config/WebSecurityConfigurer.java
+++ spring-cloud-security-introduction/auth-server/src/main/java/com/ymmihw/spring/cloud/security/introduction/auth/server/config/WebSecurityConfigurer.java
@@ -1,12 +1,11 @@
 package com.ymmihw.spring.cloud.security.introduction.auth.server.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -22,13 +21,11 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser("user").password("user").roles("USER").and()
-        .withUser("admin").password("admin").roles("USER", "ADMIN");
+    PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    auth.inMemoryAuthentication().withUser("user").password(passwordEncoder.encode("user"))
+        .roles("USER").and().withUser("admin").password(passwordEncoder.encode("admin"))
+        .roles("USER", "ADMIN");
   }
 
-  @Bean
-  public static PasswordEncoder passwordEncoder() {
-    return NoOpPasswordEncoder.getInstance();
-  }
 
 }
